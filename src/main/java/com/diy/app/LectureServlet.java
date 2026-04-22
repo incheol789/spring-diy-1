50fcd1be7bb9f07273605d8027c0005f476abbd6
정인철 <jeong-incheol@s-MacBook.local>
2026-04-22 19:37:22 +0900

    feat: LectureServlet CRUD 구현

--- src/main/java/com/diy/app/LectureServlet.java
+++ src/main/java/com/diy/app/LectureServlet.java
@@ -8,51 +8,37 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 
 @WebServlet("/lectures")
 public class LectureServlet extends HttpServlet {
 
-    public static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
-    private final Map<Long, Lecture> repository = new HashMap<>();
+    private final LectureRepository lectureRepository = new LectureRepository();
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
-        final Collection<Lecture> lectures = repository.values();
-
-        req.setAttribute("lectures", lectures);
-        req.getRequestDispatcher("lecture-list.jsp").forward(req, resp);
+        req.setAttribute("lectures", lectureRepository.findAll());
+        req.getRequestDispatcher("/lecture-list.jsp").forward(req, resp);
     }
 
     @Override
-    protected void doPost(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
-        final String body = new String(req.getInputStream().readAllBytes());
-        final Lecture lecture = OBJECT_MAPPER.readValue(body, Lecture.class);
-
-        final long id = repository.size() + 1L;
-        lecture.setId(id);
-        repository.put(lecture.getId(), lecture);
-
+    protected void doPost(final HttpServletRequest req, final HttpServletResponse resp) throws IOException {
+        final Lecture lecture = objectMapper.readValue(req.getInputStream(), Lecture.class);
+        lectureRepository.save(lecture);
         resp.sendRedirect("/lectures");
     }
 
     @Override
-    protected void doDelete(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
-        final Long id = Long.valueOf(req.getParameter("id"));
-        repository.remove(id);
-
-        resp.sendRedirect("/lectures");
+    protected void doPut(final HttpServletRequest req, final HttpServletResponse resp) throws IOException {
+        final Lecture lecture = objectMapper.readValue(req.getInputStream(), Lecture.class);
+        lectureRepository.update(lecture);
+        resp.setStatus(HttpServletResponse.SC_OK);
     }
 
     @Override
-    protected void doPut(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
-        final String body = new String(req.getInputStream().readAllBytes());
-        final Lecture lecture = OBJECT_MAPPER.readValue(body, Lecture.class);
-
-        repository.put(lecture.getId(), lecture);
-
-        resp.sendRedirect("/lectures");
+    protected void doDelete(final HttpServletRequest req, final HttpServletResponse resp) {
+        final Long id = Long.parseLong(req.getParameter("id"));
+        lectureRepository.delete(id);
+        resp.setStatus(HttpServletResponse.SC_OK);
     }
 }
