6f55fa2f812c7ad83b6188e6ecc97118b373087a
정인철 <jeong-incheol@s-MacBook.local>
2026-04-28 21:34:08 +0900

    feat: Controller 인터페이스 정의

---
+++ src/main/java/com/diy/framework/web/mvc/Controller.java
@@ -0,0 +1,9 @@
+package com.diy.framework.web.mvc;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@FunctionalInterface
+public interface Controller {
+    ModelAndView handleRequest(final HttpServletRequest request, final HttpServletResponse response) throws Exception;
+}
